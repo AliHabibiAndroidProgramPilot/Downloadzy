b8557ba346e0670219afad3c7d3f7bf45b384216
Ali Habibi <biz.alihabibi@gmail.com>
2024-07-30 11:33:24 +0330

    initial commit

---
+++ app/src/main/java/com/ir/ali/downloadzy/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.ir.ali.downloadzy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ir.ali.downloadzy.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
